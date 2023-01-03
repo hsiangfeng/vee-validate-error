ba0f68574300f365e8e5de0a07c956d0452d540b
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2023-01-03 16:27:23 +0800

    feat: 🎸 上傳會噴錯的 Vee

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue';
+
+const user = ref({
+  account: '',
+  password: '',
+});
+</script>
+
+<template>
+  <VForm
+  v-slot="{ errors }"
+  @submit="login"
+  >
+    <div>
+      <label for="account">帳號</label>
+      <VField
+        id="account"
+        v-model.trim="user.account"
+        name="帳號"
+        type="email"
+        :class="{ 'is-invalid': errors['帳號'] }"
+        rules="required|email"
+      />
+      <ErrorMessage
+        name="帳號"
+      />
+    </div>
+    <div>
+      <label for="password">密碼</label>
+      <VField
+        id="password"
+        v-model.trim="user.password"
+        name="密碼"
+        type="password"
+        :class="{ 'is-invalid': errors['密碼'] }"
+        rules="required"
+      />
+      <ErrorMessage
+        name="密碼"
+      />
+    </div>
+  </VForm>
+</template>
+
+<style scoped>
+</style>
